e71f33176dfc66f3398cffd8f4d960436920e597
Pete Chee Zachraias <pzachraias1@my.hpu.edu>
2020-11-05 15:40:10 -1000

    method added

--- hello.java
+++ hello.java
@@ -1,7 +1,12 @@
 public class hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        String msg = "Hello";
+	greeting (msg);
     }
+
+public static void greeting(String s){
+	System.out.print(s);
+}
     
 }
